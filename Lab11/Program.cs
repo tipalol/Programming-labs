d94a82076e4d4eecf6bbef49487469ca1b221b9e
sorokindmitrij <tipalol2@gmail.com>
2019-06-02 21:01:46 +0500

    Created WorkersQueue class

---
+++ Lab11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new Queue();
+            queue.ToArray();
+        }
+    }
+}
---
+++ Lab11/WorkersQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Lab10;
+
+namespace Lab11
+{
+    public class WorkersQueue : ICollection, IEnumerable, ICloneable
+    {
+        Queue workers;
+        public int Count
+        {
+            get
+            {
+                return workers.Count;
+            }
+        }
+        public bool IsSynchronized { 
+            get
+            {
+                return workers.IsSynchronized;
+            }
+        }
+        public object SyncRoot { get; }
+        public IEnumerator GetEnumerator()
+        {
+            return workers.GetEnumerator();
+        }
+        public void CopyTo(Array array, int index)
+        {
+            if (array.GetValue(index) is Person)
+                workers.CopyTo(array, index);
+            else
+                throw new Exception();
+        }
+        public object[] ToArray()
+        {
+            return workers.ToArray();
+        }
+        public object Clone()
+        {
+            return workers.Clone();
+        }
+        public bool Contains(object v)
+        {
+            return workers.Contains(v);
+        }
+        public void Clear()
+        {
+            workers.Clear();
+        }
+        public object Dequeue()
+        {
+            return workers.Dequeue();
+        }
+        public void Enqueue(object v)
+        {
+            workers.Enqueue(v);
+        }
+        public object Peek()
+        {
+            return workers.Peek();
+        }
+        public void TrimToSize()
+        {
+            workers.TrimToSize();
+        }
+        public WorkersQueue()
+        {
+            workers = new Queue();
+        }
+        public WorkersQueue(int capacity)
+        {
+            workers = new Queue(capacity);
+        }
+        public WorkersQueue(int capacity, float growFact)
+        {
+            workers = new Queue(capacity, growFact);
+        }
+        public WorkersQueue(ICollection collection)
+        {
+            workers = new Queue(collection);
+        }
+    }
+}
