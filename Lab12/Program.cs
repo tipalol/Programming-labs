1cc90be42b1a07fe5896098754587f596556516d
sorokindmitrij <tipalol2@gmail.com>
2019-06-13 22:00:28 +0500

    Ended lab 12

--- Lab12/Program.cs
+++ Lab12/Program.cs
@@ -20,12 +20,84 @@ namespace Lab12
         /// <summary>
         /// Класс с тестовыми коллекциями
         /// </summary>
-        static TestCollections testCollections;
+        static TestCollections testCollections = new TestCollections();
 
 
         public static void Main()
         {
+            int input = menu.GetChoose();
 
+            while (input != 0)
+            {
+                switch (input)
+                {
+                    case 1:
+                        int size = menu.GetInt("размер создаваемых коллекций");
+                        testCollections = new TestCollections(size);
+                        break;
+                    case 2:
+                        int gender = menu.GetInt("пол добавляемого студента");
+                        Student student = TestCollections.GenerateStudent(gender);
+                        testCollections.AddStudent(student);
+                        Console.WriteLine("Студент добавлен");
+                        break;
+                    case 3:
+                        string name = menu.GetString("имя удаляемого студента");
+                        testCollections.RemoveStudent(name);
+                        Console.WriteLine("Студент удален");
+                        break;
+                    case 4:
+                        Console.WriteLine("Нахождение первого, центрального, последнего и несуществующего элемента в коллекции List<Person>..");
+                        var watch = System.Diagnostics.Stopwatch.StartNew();
+                        testCollections.personList.Contains(testCollections.personList[0]);
+                        testCollections.personList.Contains(testCollections.personList[TestCollections.count/2]);
+                        testCollections.personList.Contains(testCollections.personList[TestCollections.count-2]);
+                        testCollections.personList.Contains(new Person("Nothing", 228));
+                        watch.Stop();
+                        var elapsedMs = watch.ElapsedMilliseconds;
+                        Console.WriteLine($"Затраченное время: {elapsedMs} миллисекунд");
+                        Console.WriteLine("Нахождение первого, центрального, последнего и несуществующего элемента в коллекции List<string>..");
+                        watch = System.Diagnostics.Stopwatch.StartNew();
+                        testCollections.stringList.Contains(testCollections.stringList[0]);
+                        testCollections.stringList.Contains(testCollections.stringList[TestCollections.count / 2]);
+                        testCollections.stringList.Contains(testCollections.stringList[TestCollections.count - 2]);
+                        testCollections.stringList.Contains("Nothing");
+                        watch.Stop();
+                        elapsedMs = watch.ElapsedMilliseconds;
+                        Console.WriteLine($"Затраченное время: {elapsedMs} миллисекунд");
+                        Console.WriteLine("Нахождение первого, центрального, последнего и несуществующего элемента в коллекции Dictionary<string, Student>..");
+                        watch = System.Diagnostics.Stopwatch.StartNew();
+                        testCollections.stringStudentDictionary.ContainsKey(testCollections.stringList[0]);
+                        testCollections.stringStudentDictionary.ContainsKey(testCollections.stringList[TestCollections.count / 2]);
+                        testCollections.stringStudentDictionary.ContainsKey(testCollections.stringList[TestCollections.count - 2]);
+                        testCollections.stringStudentDictionary.ContainsKey("Nothing");
+                        testCollections.stringStudentDictionary.ContainsValue(new Student("Something", 228, 10));
+                        watch.Stop();
+                        elapsedMs = watch.ElapsedMilliseconds;
+                        Console.WriteLine($"Затраченное время: {elapsedMs} миллисекунд");
+                        Console.WriteLine("Нахождение первого, центрального, последнего и несуществующего элемента в коллекции Dictionary<Person, Student>..");
+                        watch = System.Diagnostics.Stopwatch.StartNew();
+                        testCollections.personStudentDictionary.ContainsKey(testCollections.personList[0]);
+                        testCollections.personStudentDictionary.ContainsKey(testCollections.personList[TestCollections.count / 2]);
+                        testCollections.personStudentDictionary.ContainsKey(testCollections.personList[TestCollections.count - 2]);
+                        testCollections.personStudentDictionary.ContainsKey(new Person("Nothing", 228));
+                        testCollections.personStudentDictionary.ContainsValue(new Student("Something", 228, 10));
+                        watch.Stop();
+                        elapsedMs = watch.ElapsedMilliseconds;
+                        Console.WriteLine($"Затраченное время: {elapsedMs} миллисекунд");
+                        break;
+                    default:
+                        Console.WriteLine("Something goes wrong :(");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+                testCollections.Print();
+                Console.ReadKey();
+                Console.Clear();
+                input = menu.GetChoose();
+                Console.Clear();
+            }
         }
     }
 }
--- Lab12/TestCollections.cs
+++ Lab12/TestCollections.cs
@@ -4,28 +4,52 @@ namespace Lab12
 {
     public class TestCollections
     {
-        readonly List<Person> personList;
-        readonly List<string> stringList;
-        readonly Dictionary<string, Student> stringStudentDictionary;
-        readonly Dictionary<Person, Student> personStudentDictionary;
-        static int count = 0;
+        public readonly List<Person> personList;
+        public readonly List<string> stringList;
+        public readonly Dictionary<string, Student> stringStudentDictionary;
+        public readonly Dictionary<Person, Student> personStudentDictionary;
+        /// <summary>
+        /// Кол-во студентов в коллекциях
+        /// </summary>
+        public static int count = 0;
+        /// <summary>
+        /// Генерирует студента указанного пола
+        /// </summary>
+        /// <returns>The student.</returns>
+        /// <param name="gender">Gender.</param>
         public static Student GenerateStudent(int gender)
         {
             var random = new Random();
             count++;
             return new Student($"Студент №{count}", gender, random.Next(1,4));
         }
+        /// <summary>
+        /// Добавляет студента в коллекцию
+        /// </summary>
+        /// <param name="student">Student.</param>
         public void AddStudent(Student student)
         {
-            stringList.Add(student.Name);
-            personList.Add(student.BasePerson);
-            stringStudentDictionary.Add(student.Name, student);
-            personStudentDictionary.Add(student.BasePerson, student);
+            try
+            {
+                if (stringList == null) throw new NullReferenceException("Коллекции не были проинициализированы");
+                stringList.Add(student.Name);
+                personList.Add(student.BasePerson);
+                stringStudentDictionary.Add(student.Name, student);
+                personStudentDictionary.Add(student.BasePerson, student);
+            } catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+        /// <summary>
+        /// Удаляет студента из коллекции
+        /// </summary>
+        /// <param name="name">Name.</param>
         public void RemoveStudent(string name)
         {
             try
             {
+                if (stringList == null) throw new NullReferenceException("Коллекции не были проинициализированы");
                 if (stringList.Contains(name))
                 {
                     stringList.Remove(name);
@@ -38,7 +62,18 @@ namespace Lab12
             } catch (DidntExistElementException e)
             {
                 Console.WriteLine(e);
+            } catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
             }
+        }
+        /// <summary>
+        /// Выводит содержимое коллекции на экран
+        /// </summary>
+        public void Print()
+        {
+            foreach (Student student in stringStudentDictionary.Values)
+                Console.WriteLine(student.TellAbout());
         }
         public TestCollections(int count = 0)
         {
